433d4d6d68d739966310dcae08388076bb505673
amrazek <adam.mrazel@mgm-tp.com>
2017-08-14 01:14:03 +0200

    Test Jenkinsu

--- src/app/modules/login/components/login/login.component.ts
+++ src/app/modules/login/components/login/login.component.ts
@@ -9,7 +9,7 @@ import {AuthService} from '../../../shared/services/auth.service';
     host: {
         '[attr.fxFill]': '100'
     },
-})
+}
 export class LoginComponent implements OnInit {
     @HostBinding('class') classes = 'width-100 height-100';
 
@@ -26,12 +26,15 @@ export class LoginComponent implements OnInit {
         });
     }
 
+     
+
     ngOnInit(): void {
         this.formGroup = new FormGroup({
             username: new FormControl(),
             password: new FormControl()
         });
     }
+
 
     login(): void {
         this.authService.loginFacebook().subscribe((isAuthenticated) => {

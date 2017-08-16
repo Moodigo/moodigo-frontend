795bac1e505a5bbe100132d9496bbbbb97781333
amrazek <adam.mrazel@mgm-tp.com>
2017-08-16 21:51:57 +0200

    Switch Origin CORS

--- mock-server/index.js
+++ mock-server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dataProvider = require('./data-provider');
 const app = express();
 
-app.listen(9002, listening);
+let server = app.listen(9002, listening);
 
 function listening() {
     console.log('Server is listening on http://localhost:9002/');
@@ -11,7 +11,11 @@ function listening() {
 app.set('json spaces', 2);
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    let allowOrigin = 'http://zoutly.com';
+    if (req.headers.host === 'localhost:9002') {
+        allowOrigin = "http://localhost:4200";
+    }
+    res.header("Access-Control-Allow-Origin", allowOrigin);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-Auth-Token");
     res.header("Access-Control-Allow-METHODS", "GET, OPTIONS, PATCH, POST");
     res.header("Access-Control-Allow-Credentials", "true");
